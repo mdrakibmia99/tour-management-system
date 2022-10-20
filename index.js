a34466785f8a9a6398ffb9936f647a4e1c15a0da
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2022-10-20 16:56:47 +0600

    external all import setup done

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * Title: Tour Management System
+ * Description: Create a system that contains several endpoints.
+ * Author: Md Rakib Mia
+ */
+
+/* external imports */
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
