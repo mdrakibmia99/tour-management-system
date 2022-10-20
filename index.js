4483b014b9edc0acc642b25f6c3df8ecb59eabbf
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2022-10-20 17:15:19 +0600

    index.js file workd done

--- index.js
+++ index.js
@@ -9,19 +9,51 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
+
+/* internal imports */
+const errorHandler = require("./middlewares/error.middleware");
+// const dbConnection = require("./utils/db.util");
+const toursRouter = require("./routes/tours.route");
+const tourRouter = require("./routes/tour.route");
+
 /* application connection */
 const app = express();
 const port = process.env.PORT;
+
 /* middleware connection */
 app.use(cors());
 app.use(express.json());
 
+/* router connection */
+app.use("/tours", toursRouter);
+app.use("/tour", tourRouter);
+
+/* global error handler */
+app.use(errorHandler);
+
+/* DB connection */
+// dbConnection();
+mongoose
+  .connect(process.env.DB_URI, {
+    dbName: "tour-management-system",
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("TMS connect with MongoDB through Mongoose.");
+  });
+
 /* enable backend */
 app.get("/", (req, res) => {
-    res.status(200).json({
-      acknowledgement: true,
-      title: "Tour Management System",
-      message: "OK",
-      description: "TMS request on server working.",
-    });
+  res.status(200).json({
+    acknowledgement: true,
+    title: "Tour Management System",
+    message: "OK",
+    description: "TMS request on server working.",
   });
+});
+
+/* enable port */
+app.listen(port, () => {
+  console.log(`TMS listening on port ${port}.`);
+});
